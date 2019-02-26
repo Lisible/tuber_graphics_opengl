f820aaa32fe7f578536ae86fb852393d20636be4
Clément Sibille <clement.sibille@protonmail.com>
2019-02-26 15:17:02 +0100

    Add Drop trait implementation for Texture

--- src/opengl.rs
+++ src/opengl.rs
@@ -212,6 +212,12 @@ impl Texture {
     }
 }
 
+impl Drop for Texture {
+    fn drop(&mut self) {
+        unsafe { gl::DeleteTextures(1, &self.id); }
+    }
+}
+
 pub enum TextureParameterValue {
     Int(gl::types::GLint),
     Float(gl::types::GLfloat),
