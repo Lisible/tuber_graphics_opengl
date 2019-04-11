ea7ac526b453d0c3f51d29935c15e7ec72ffcc57
Clément SIBILLE <claymeuns@protonmail.com>
2019-04-11 06:58:43 +0200

    Add Font, FontMetadata and FontCharacterMetadata structs

--- src/lib.rs
+++ src/lib.rs
@@ -370,7 +370,123 @@ impl RenderBatch {
     }
 }
 
-/// Represents a mesh
+use std::collections::HashMap;
+
+pub struct Font {
+    metadata: FontMetadata,
+    texture: opengl::Texture
+}
+
+impl Font {
+    pub fn new(texture: opengl::Texture) -> Font {
+        Font {
+            metadata: FontMetadata::new(),
+            texture
+        }
+    }
+
+    pub fn add_character(&mut self, character: char,
+                         metadata: FontCharacterMetadata) {
+        self.metadata.add_character(character, metadata);
+    }
+
+    pub fn metadata(&self) -> &FontMetadata {
+        &self.metadata
+    }
+
+    pub fn bind_texture(&self) {
+        self.texture.bind();
+    }
+
+    pub fn unbind_texture(&self) {
+        self.texture.unbind();
+    }
+}
+
+pub struct FontMetadata {
+    characters: HashMap<char, FontCharacterMetadata>,
+}
+
+impl FontMetadata {
+    pub fn new() -> FontMetadata {
+        FontMetadata {
+            characters: HashMap::new()
+        }
+    }
+
+    pub fn add_character(&mut self, character: char,
+                         metadata: FontCharacterMetadata) {
+        self.characters.insert(character, metadata);
+    }
+
+    pub fn character(&self, character: char)  -> Option<&FontCharacterMetadata> {
+        self.characters.get(&character)
+    }
+}
+
+pub struct FontCharacterMetadata {
+    x_coordinate: f32,
+    y_coordinate: f32,
+    width: f32,
+    height: f32,
+    x_offset: f32,
+    y_offset: f32,
+    x_advance: f32,
+    page: usize,
+    channel: u8
+}
+
+impl FontCharacterMetadata {
+    pub fn new(x_coordinate: f32, y_coordinate: f32, width: f32, height: f32,
+               x_offset: f32, y_offset: f32, x_advance: f32, page: usize,
+               channel: u8) -> FontCharacterMetadata {
+        FontCharacterMetadata {
+            x_coordinate,
+            y_coordinate,
+            width,
+            height,
+            x_offset,
+            y_offset,
+            x_advance,
+            page,
+            channel
+        }
+    }
+
+    pub fn x_coordinate(&self) -> f32 {
+        self.x_coordinate
+    }
+    pub fn y_coordinate(&self) -> f32 {
+        self.y_coordinate
+    }
+
+    pub fn width(&self) -> f32 {
+        self.width
+    }
+    pub fn height(&self) -> f32 {
+        self.height
+    }
+
+    pub fn x_offset(&self) -> f32 {
+        self.x_offset
+    }
+    pub fn y_offset(&self) -> f32 {
+        self.y_offset
+    }
+
+    pub fn x_advance(&self) -> f32 {
+        self.x_advance
+    }
+
+    pub fn page(&self) -> usize {
+        self.page
+    }
+
+    pub fn channel(&self) -> u8 {
+        self.channel
+    }
+}
+
 #[derive(Clone)]
 pub struct Mesh {
     vertices: Vec<Vertex>,
